217bfe1e3ce6585582dd92422c1b346591051e91
Rusdy0201 <rusdynsh@gmail.com>
2020-04-22 19:30:14 +0700

    complete basic code for haruka

--- main.py
+++ main.py
@@ -10,3 +10,46 @@ def speak(audioString):
     tts = gTTS(text = audioString, lang = 'en')
     tts.save("speak.mp3")
     os.systems("mpg321 speak.mp3")
+
+def recordAudio():
+    r = SR.Recognizer()
+
+    with SR.Microphone() as source:
+        print("say something !")
+        audio = r.listen(source)
+
+    command = ""
+
+    try:
+        command = r.recognize_google(audio)
+        print("you said: " + command)
+    except SR.UnknownValueError:
+        print("google speech recognition could not understand audio")
+    except SR.RequestError as e:
+        print("could not request result from google speech recognition service; {0}".format(e))
+
+    return command
+
+def haruka(command):
+
+    if "hi" in command:
+        speak("hallo")
+        PS("speak.mp3")
+
+    if "what is your name" in command:
+        speak("my name is haruka")
+        PS("speak.mp3")
+
+    elif "who is your parents" in command:
+        speak("her name is rusdiansyah")
+        PS("speak.mp3")
+
+
+
+time.sleep(2)
+
+while True:
+
+    command = recordAudio()
+
+    haruka(command)
