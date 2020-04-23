76dd00ce8512e135303cc0cf7f55bf884ce37a07
Rusdy0201 <rusdynsh@gmail.com>
2020-04-23 22:45:38 +0700

    update

--- devControl.py
+++ devControl.py
@@ -4,35 +4,13 @@ from pyfirmata import Arduino, util
 
 board = Arduino ('/dev/ttyACM0')
 
+led_pin = 13
 it = util.Iterator(board)
 it.start()
 
-#define pin Digital Input Arduino
-d1Input = board.get_pin('d:1:i')
-d2Input = board.get_pin('d:2:i')
-d3Input = board.get_pin('d:3:i')
-d4Input = board.get_pin('d:4:i')
-d5Input = board.get_pin('d:5:i')
-d6Input = board.get_pin('d:6:i')
-d7Input = board.get_pin('d:7:i')
-d8Input = board.get_pin('d:8:i')
-d9Input = board.get_pin('d:9:i')
-d10Input = board.get_pin('d:10:i')
-d11Input = board.get_pin('d:11:i')
-d12Input = board.get_pin('d:12:i')
-d13Input = board.get_pin('d:13:i')
-
-#define pin Digital Output Arduino
-d1Output = board.get_pin('d:1:o')
-d2Output = board.get_pin('d:2:o')
-d3Output = board.get_pin('d:3:o')
-d4Output = board.get_pin('d:4:o')
-d5Output = board.get_pin('d:5:o')
-d6Output = board.get_pin('d:6:o')
-d7Output = board.get_pin('d:7:o')
-d8Output = board.get_pin('d:8:o')
-d9Output = board.get_pin('d:9:o')
-d10Output = board.get_pin('d:10:o')
-d11Output = board.get_pin('d:11:o')
-d12Output = board.get_pin('d:12:o')
-d13Output = board.get_pin('d:13:o')
+def blink():
+    while True:
+        board.digital[led_pin].write(1)
+        board.pass_time(1)
+        board.digital[led_pin].write(0)
+        board.pass_time(1)
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
 import time
 import speech_recognition as SR
-import devControl
-from gtts import gtts
+import devControl as dv
+from gtts import gTTS
 from playsound import playsound as PS
 
 def speak(audioString):
     print(audioString)
     tts = gTTS(text = audioString, lang = 'en')
     tts.save("speak.mp3")
-    os.systems("mpg321 speak.mp3")
+    os.system("mpg321 speak.mp3")
 
 def recordAudio():
     r = SR.Recognizer()
@@ -32,16 +32,16 @@ def recordAudio():
 
 def haruka(command):
 
-    if "hi" in command:
-        speak("hallo")
+    if "hi Haruka" in command:
+        speak("hallo dad")
         PS("speak.mp3")
 
-    if "what is your name" in command:
-        speak("my name is haruka")
+    if "where your mom" in command:
+        speak("i don't know dad , maybe she was going out")
         PS("speak.mp3")
 
-    elif "who is your parents" in command:
-        speak("her name is rusdiansyah")
+    elif "can you help me" in command:
+        speak("of course dad , what can i do for you")
         PS("speak.mp3")
 
 
